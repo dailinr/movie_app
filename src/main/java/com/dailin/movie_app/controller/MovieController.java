9ccdca635e1be5cdce7b0e1afb27a67d80e19ce4
Dailin Romero <dailinromero123@gmail.com>
2025-09-02 17:11:12 -0500

    enpoint con @PathVariable y filtrado con @RequestParams

--- src/main/java/com/dailin/movie_app/controller/MovieController.java
+++ src/main/java/com/dailin/movie_app/controller/MovieController.java
@@ -3,12 +3,16 @@ package com.dailin.movie_app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dailin.movie_app.persistence.entity.Movie;
 import com.dailin.movie_app.service.MovieService;
+import com.dailin.movie_app.util.MovieGenre;
 
 @RestController
 @RequestMapping("/movies")
@@ -18,8 +22,31 @@ public class MovieController {
     private MovieService movieService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Movie> findAll() {
-        System.out.println("Entro al metodo findAll de movieController");
-        return movieService.findAll();
+    public List<Movie> findAll(@RequestParam(required = false) String title, 
+        @RequestParam(required = false) MovieGenre genre) 
+    {
+
+        List<Movie> peliculas = null;
+
+        // validamos que haya un valor para variable title y que no este vacio con espacione en blanco
+        if(StringUtils.hasText(title) && genre != null) {
+            peliculas = movieService.findAllByGenreAndTitle(genre, title);
+        }
+        else if(StringUtils.hasText(title)) {
+            peliculas = movieService.findAllByTitle(title);
+        }
+        else if(genre != null){
+            peliculas = movieService.findAllByGenre(genre);
+        }
+        else {
+            peliculas = movieService.findAll();
+        }
+
+        return peliculas;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public Movie findOneById(@PathVariable Long id) {
+        return movieService.findOneById(id);
     }
 }
--- src/main/java/com/dailin/movie_app/controller/UserController.java
+++ src/main/java/com/dailin/movie_app/controller/UserController.java
@@ -3,8 +3,11 @@ package com.dailin.movie_app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dailin.movie_app.persistence.entity.User;
@@ -18,7 +21,22 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<User> findAll() {
-        return userService.findAll();
+    public List<User> findAll(@RequestParam(required = false) String name) {
+
+        List<User> usuarios = null;
+
+        if(StringUtils.hasText(name)) {
+            usuarios = userService.findAllByName(name);
+        }
+        else{
+            usuarios = userService.findAll();
+        }
+
+        return usuarios;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{username}")
+    public User findOneByUsername (@PathVariable String username) {
+        return userService.findOneByUsername(username);
     }
 }
