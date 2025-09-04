dec258773076c1bb0e09e9bf6b81e2123527ef98
Dailin Romero <dailinromero123@gmail.com>
2025-09-03 19:19:30 -0500

    CRUD para userController

--- src/main/java/com/dailin/movie_app/controller/UserController.java
+++ src/main/java/com/dailin/movie_app/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.dailin.movie_app.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dailin.movie_app.exception.ObjectNotFoundException;
 import com.dailin.movie_app.persistence.entity.User;
 import com.dailin.movie_app.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/users")
@@ -47,5 +51,43 @@ public class UserController {
             return ResponseEntity.notFound().build(); 
         }
         
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<User> createOne(@RequestBody User user, HttpServletRequest request) {
+       
+        User userCreated = userService.createOne(user);
+        String baseUrl = request.getRequestURL().toString();
+        // creamos la nueva localizacion paara acceder al documento recien creado
+        URI newLocation = URI.create(baseUrl+"/"+userCreated.getId());
+
+
+        return ResponseEntity
+            .created(newLocation)
+            .body(userCreated);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{username}")
+    public ResponseEntity<User> updateOneByUsername(@PathVariable String username, @RequestBody User user){
+
+        try {
+            User userUpdated = userService.updatedOneByUsername(username, user);
+            return ResponseEntity.ok(userUpdated);
+        } 
+        catch (ObjectNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{username}")
+    public ResponseEntity<Void> deleteOneByUsername(@PathVariable String username) {
+        
+        try {
+            userService.deleteOneByUsername(username);
+            return ResponseEntity.noContent().build();
+        } 
+        catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/dailin/movie_app/persistence/repository/UserCrudRepository.java
+++ src/main/java/com/dailin/movie_app/persistence/repository/UserCrudRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dailin.movie_app.persistence.entity.User;
 
@@ -17,5 +18,6 @@ public interface UserCrudRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     @Modifying // especificamos que este metodo no será una consulta
+    @Transactional  // ya que es un query definido por mi, debo indicar que es transactional si no es de consulta
     int deleteByUsername(String username);
 }
