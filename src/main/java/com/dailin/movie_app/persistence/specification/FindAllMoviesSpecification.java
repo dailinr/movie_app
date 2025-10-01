4aae918d7fc63623f67d01b363be07709bfbda87
Dailin Romero <dailinromero123@gmail.com>
2025-10-01 18:46:46 -0500

    Operador IN con Specifications JPA

--- src/main/java/com/dailin/movie_app/persistence/specification/FindAllMoviesSpecification.java
+++ src/main/java/com/dailin/movie_app/persistence/specification/FindAllMoviesSpecification.java
@@ -1,6 +1,7 @@
 package com.dailin.movie_app.persistence.specification;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.data.jpa.domain.Specification;
@@ -9,7 +10,6 @@ import org.springframework.util.StringUtils;
 import com.dailin.movie_app.dto.request.MovieSearchCriteria;
 import com.dailin.movie_app.persistence.entity.Movie;
 import com.dailin.movie_app.persistence.entity.Rating;
-import com.dailin.movie_app.util.MovieGenre;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -35,17 +35,18 @@ public class FindAllMoviesSpecification implements Specification<Movie> {
         // esta será la lista de predicados
         List<Predicate> predicates = new ArrayList<>();
 
+        if(StringUtils.hasText(this.searchCriteria.title())){
+            // nombre atributo de la entidad y el valor de la busqueda (viene desde el controlador)
+            Predicate titleLike = criteriaBuilder.like(root.get("title"), "%"+this.searchCriteria.title()+"%");
+            // m.title like '%sjdjdj%'
+
+            predicates.add(titleLike);
+        }
+
         if(this.searchCriteria.genres() != null && this.searchCriteria.genres().length > 0) {
-            List<Predicate> genrePredicates = new ArrayList<>(); 
-
-            for(MovieGenre genre : this.searchCriteria.genres()){
-
-                Predicate genreEqual = criteriaBuilder.equal(root.get("genre"), genre);
-                genrePredicates.add(genreEqual);
-            }
-
-            Predicate genreEqual = criteriaBuilder.or(genrePredicates.toArray(new Predicate[0]));
-            // and (m.genre = ?2.1 OR m.genre = ?2.2 OR m.genre = ?2.3 OR m.genre = ?2.4)
+            // debemos convertir el listado de generos, de un arreglo a un List<>
+            Predicate genreEqual = criteriaBuilder.in(root.get("genre")).value(Arrays.stream(this.searchCriteria.genres()).toList()); 
+            // and m.genre in (?, ?, ?)
             predicates.add(genreEqual);
         }
 
@@ -81,24 +82,13 @@ public class FindAllMoviesSpecification implements Specification<Movie> {
         }
         
         // necesitamos convertir la lista de predicados a un arreglo
-        Predicate predicateWithAnd = criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
 
-        if(StringUtils.hasText(this.searchCriteria.title())){
-            // nombre atributo de la entidad y el valor de la busqueda (viene desde el controlador)
-            Predicate titleLike = criteriaBuilder.like(root.get("title"), "%"+this.searchCriteria.title()+"%");
-            // m.title like '%sjdjdj%'
-
-            // se filtrará por el titulo OR el listado de predicados por and
-            return criteriaBuilder.or(titleLike, predicateWithAnd);
-        }
-
-        // en caso de que no hay un filtro por titulo
-        return predicateWithAnd;
 
         // select m.* 
         // from movie m 
         // where 1 = 1 and m.title like '%1%' 
-        //             OR (m.genre =?2.1 or m.genre = ?2.2 or m.genre = ?2.3)
+        //             and m.genre in (?, ? , ?)
         //             and m.releaseYear >= ?3
         //             and m.realseYear <= ?4
         //             and (SELECT avg(r1_0.rating) 
